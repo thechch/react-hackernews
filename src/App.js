10652c645ca26118571d51986de439691bc3fe50
Anton Serous <thech.ch@gmail.com>
2017-01-24 02:28:49 +1300

    styles added, beginning of api call implementation

--- src/App.js
+++ src/App.js
@@ -21,16 +21,28 @@ const list = [
     },
 ];
 
+const DEFAULT_QUERY =  'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+var url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
+console.log(url);
+
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            list,
-            searchTerm: '',
+            result: null,
+            searchTerm: DEFAULT_QUERY,
         };
 
+        this.setSearchTopstories = this.setSearchTopstories.bind(this);
+        this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
         this.onDismiss = this.onDismiss.bind(this);
         this.onSearchChange = this.onSearchChange.bind(this);
     }
@@ -50,6 +62,20 @@ class App extends Component {
         });
     }
 
+    setSearchTopstories(result){
+        this.setState(result);
+    }
+
+    fetchSearchTopstories(searchTerm) {
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+            .then(response => response.json())
+            .then(result => this.setSearchTopstories(result));
+    }
+
+    componentDidMount() {
+        const { searchTerm }  = this.state;
+        this.fetchSearchTopstories(searchTerm);
+    }
     render() {
         const {list, searchTerm} = this.state;
 
--- src/Table.js
+++ src/Table.js
@@ -4,19 +4,27 @@ import Button from './Button';
 const isSearched = (searchTerm) => (item) =>
 !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
+const largeColumn = { width: '40%',
+};
+const midColumn = { width: '30%',
+};
+const smallColumn = { width: '10%',
+};
+
 const Table = ({list, pattern, onDismiss}) =>
-        <div>
+        <div className="table">
             {list.filter(isSearched(pattern)).map(item =>
-                <div key={item.objectID}>
-                        <span>
+                <div key={item.objectID} className="table-row">
+                        <span style={largeColumn}>
                           <a href={item.url}>{item.title} </a>
                         </span>
-                    <span>{item.author} </span>
-                    <span>{item.num_comments} </span>
-                    <span>{item.points} </span>
-                    <span>
+                    <span style={midColumn}>{item.author} </span>
+                    <span style={smallColumn}>{item.num_comments} </span>
+                    <span style={smallColumn}>{item.points} </span>
+                    <span style={smallColumn}>
                             <Button
                                 onClick={() => onDismiss(item.objectID)}
+                                className="button-inline"
                             >
                                 Dismiss
                             </Button>
